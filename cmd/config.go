e2463e676601bf512570a6cb8ebf36ba65532097
d-kuro <kurosawa7620@gmail.com>
2019-09-17 15:43:25 +0900

    Fix bug for nodes command.

--- cmd/config.go
+++ cmd/config.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"io/ioutil"
+	"os"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -12,8 +13,24 @@ type Config struct {
 	AutoScalingGroup string `yaml:"auto_scaling_group"`
 }
 
+type ErrFileNotExist struct {
+	message string
+}
+
+func (e ErrFileNotExist) Error() string {
+	return e.message
+}
+
 func GetConfig(configFile string) (*Config, error) {
-	buf, err := ioutil.ReadFile(configFile)
+	f, err := os.OpenFile(configFile, os.O_RDONLY, 0)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, &ErrFileNotExist{message: err.Error()}
+		}
+		return nil, err
+	}
+
+	buf, err := ioutil.ReadAll(f)
 	if err != nil {
 		return nil, err
 	}
--- cmd/nodes.go
+++ cmd/nodes.go
@@ -1,15 +1,17 @@
 package cmd
 
 import (
+	"errors"
+
 	"github.com/d-kuro/escale/pkg/log"
 	"github.com/github/vulcanizer"
 	"github.com/spf13/cobra"
 )
 
 type NodesOption struct {
-	host   string
-	port   int
-	config string
+	host       string
+	port       int
+	configFile string
 }
 
 func NewNodesCommand(o *NodesOption) *cobra.Command {
@@ -23,13 +25,16 @@ func NewNodesCommand(o *NodesOption) *cobra.Command {
 	fset := cmd.Flags()
 	fset.StringVar(&o.host, "host", "", "Elasticsearch host")
 	fset.IntVar(&o.port, "port", 9200, "Elasticsearch port")
-	fset.StringVarP(&o.host, "config", "f", ".escale.yaml", "Configuration file to read in")
+	fset.StringVarP(&o.configFile, "config_file", "f", ".escale.yaml", "Configuration file to read in")
 	return cmd
 }
 
 func runNodesCommand(o *NodesOption) error {
 	o, err := o.readConfig()
 	if err != nil {
+		return err
+	}
+	if err := o.validate(); err != nil {
 		return err
 	}
 
@@ -46,13 +51,19 @@ func runNodesCommand(o *NodesOption) error {
 }
 
 func (o NodesOption) readConfig() (*NodesOption, error) {
-	if o.config == "" {
+	if o.configFile == "" {
 		return &o, nil
 	}
-	config, err := GetConfig(o.config)
+	config, err := GetConfig(o.configFile)
 	if err != nil {
-		return nil, err
+		switch err.(type) {
+		case *ErrFileNotExist:
+			return &o, nil
+		default:
+			return nil, err
+		}
 	}
+
 	if o.host == "" {
 		o.host = config.Host
 	}
@@ -60,4 +71,11 @@ func (o NodesOption) readConfig() (*NodesOption, error) {
 		o.port = config.Port
 	}
 	return &o, nil
+}
+
+func (o NodesOption) validate() error {
+	if o.host == "" {
+		return errors.New("host is required")
+	}
+	return nil
 }
